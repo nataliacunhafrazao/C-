92255bdefa88dbd2221adddd018e04a6d7ac57e2
Natália Frazão <108697207+nataliacunhafrazao@users.noreply.github.com>
2025-11-27 21:14:38 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿class Programa
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Exercício 2- Fixando o laço wile");
+
+        Console.WriteLine("Digite seu nome ");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Olá " + nome); Console.WriteLine("Digite a sua idade");
+        int idade = Console.Read();
+        Console.Read();
+        int.TryParse(Console.ReadLine(), out idade);
+
+       
+
+        Console.WriteLine("tecle enter para fechar");
+        Console.ReadLine();
+
+
+
+    }
+}
+
+
